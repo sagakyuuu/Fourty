72e39a2dacfd54c6f5a6425ef9cfaa0d629057e9
sagakyuuu <sagalawiliam0@gmail.com>
2025-06-11 21:51:23 +0700

    test:edit

--- src/test/ProductTest.java
+++ src/test/ProductTest.java
@@ -3,6 +3,7 @@ package test;
 public class ProductTest {
 
     public static void main(String[] args) {
+        System.out.println("Hello World");
         new controller.Product().getProduct();
     }
 }
